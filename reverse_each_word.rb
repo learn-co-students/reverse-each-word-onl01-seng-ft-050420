45b80e2e7c813d1f85c5940eaa6c5c066345e308
Jackson Ramos <jackramos021495@gmail.com>
2020-05-05 22:04:38 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  #sentence.split
+ array = sentence.split(" ")
+ new_array = []
+  array.collect do |word|
+    new_array << word.reverse
+  end
+  new_array.join(" ")
+end
